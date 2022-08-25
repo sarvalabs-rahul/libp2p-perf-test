6cc4455b83e2cffc087602d5316fb28baec22822
Marten Seemann <martenseemann@gmail.com>
2022-08-25 13:49:55 +0300

    chore: update go-libp2p to v0.22.0 (#4)

--- cmd/test-client/main.go
+++ cmd/test-client/main.go
@@ -15,11 +15,10 @@ import (
 
 	manet "github.com/multiformats/go-multiaddr/net"
 
-	"github.com/libp2p/go-libp2p-core/network"
-	"github.com/libp2p/go-libp2p-core/peer"
-	"github.com/libp2p/go-libp2p-core/protocol"
-
 	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/protocol"
 	"github.com/libp2p/go-libp2p/p2p/muxer/mplex"
 	"github.com/libp2p/go-libp2p/p2p/muxer/yamux"
 	"github.com/libp2p/go-libp2p/p2p/security/noise"
--- cmd/test-server/main.go
+++ cmd/test-server/main.go
@@ -9,11 +9,10 @@ import (
 	"net"
 	"net/http"
 
-	"github.com/libp2p/go-libp2p-core/crypto"
-	"github.com/libp2p/go-libp2p-core/network"
-	"github.com/libp2p/go-libp2p-core/protocol"
-
 	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p/core/crypto"
+	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/libp2p/go-libp2p/core/protocol"
 	"github.com/libp2p/go-libp2p/p2p/muxer/mplex"
 	"github.com/libp2p/go-libp2p/p2p/muxer/yamux"
 	quic "github.com/libp2p/go-libp2p/p2p/transport/quic"
@@ -23,8 +22,6 @@ import (
 )
 
 const TestProtocol = protocol.ID("/libp2p/test/data")
-
-var testFilePath string
 
 var randomData []byte
 
